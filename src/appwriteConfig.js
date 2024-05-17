e387d090883c9fda488d0d38b22de5ae8064ed76
Ankit Vishwakarma <ankitvishwakarma3354@gmail.com>
2024-05-17 12:39:07 +0530

    update

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -1,10 +1,9 @@
 import { Client, Databases } from "appwrite";
 
+export const AppwriteURL =
+  import.meta.env.VITE_APPWRITE_URL || "https://cloud.appwrite.io/v1";
 export const PORJECT_ID =
   import.meta.env.VITE_APPWRITE_DATABASES_ID || "66387c2d00392835a51c";
-export const AppwriteURL =
-  import.meta.env.VITE_APPWRITE_URL ||
-  "https://cloud.appwrite.io/v1";
 export const DATABASES_ID =
   import.meta.env.VITE_APPWRITE_DATABASES_ID || "66387c50002eb78ca32f";
 export const COLLECTION_MESSAGE_ID =
